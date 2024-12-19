a341b3c803a408ba413ec896c8d04b7b39f13a74
paoladjobissie <paoladjobissie@gmail.com>
2024-12-18 19:28:21 -0500

    Add some modifications

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,7 @@
-# Create a resource group in EastUS region - Uses Default Provider
-resource "azurerm_resource_group" "myrg1" {
-  name = "myrg-1"
-  location = "East US"
+variable "resource_group_location" {
+  default     = "westeurope"
+  description = "Location of the resource group."
 }
-
-#Create a resource group in WestUS region - Uses "provider2-westus" provider
-resource "azurerm_resource_group" "myrg2" {
-  name = "myrg-2"
-  location = "West US"
-  provider = azurerm.provider2-westus
-}
-
 
 variable "resource_group_name_prefix" {
   default     = "rg"
