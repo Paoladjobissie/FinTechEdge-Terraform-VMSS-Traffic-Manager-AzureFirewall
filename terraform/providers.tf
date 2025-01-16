c47e7d511542d8295e28b7ed0babac3d329e8beb
Paola Djobissie <Djobissiepaola@gmail.com>
2025-01-16 00:05:07 -0500

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
 
-  subscription_id = "659c9195-eeff-450f-86f0-11ec1780f7bb"
+  subscription_id = "Your Azure Subscription"
   client_id       = "dfdbf619-6141-4c95-8995-86c173018340"
   client_secret    = "MYY8Q~QFnlZsO7M0anjZs5kyONFJizWZOCtLIatk"
   tenant_id       = "d3abbf87-4aaf-45e8-93ac-9eb6d1a56aa4"
